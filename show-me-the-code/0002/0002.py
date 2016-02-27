4cba6c9ac6cf62d52fa45f0a0a3ad4fca95cc173
NUM_24 <1018810506@qq.com>
2016-02-27 23:42:28 +0800

    0002代码提交 Python3.5装不上MySQL库还是个问题

---
+++ show-me-the-code/0002/0002.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import mysql.connector
+
+#从文件中读取信息
+def file_read(filename):
+	with open(filename,'r') as f:
+		lines = f.readlines()
+	for n in range(0,200):
+		lines[n] = lines[n].strip()
+	return lines
+
+def mysql_write():
+	lines = file_read("result.txt")
+	conn = mysql_connector.connect(user = 'root', password = '12345', database = 'test', use_unicode = True)
+	cursor = conn.cursor()
+	cursor.execute("creat table promo_code (id int(3) primary key, number varchar(8))")
+	for i in range(0, 200):
+		cursor.execute('insert into promo_code (id, number) values (%s, %s)', [n+1, lines[n]])
+	conn.commit()
+	cursor.close()
+	conn.close()
+	print(finish)
+
+if __name__ == '__main__':
+	mysql_write()
